177c8f45dd860c2cb095de87324961e86e8b2ae1
FilipooV <82979355+FilipooV@users.noreply.github.com>
2024-12-26 01:48:16 +0100

    Update method

--- Mail/ReminderNotification.cs
+++ Mail/ReminderNotification.cs
@@ -9,20 +9,34 @@ namespace LicytacjeSraper
 {
     public class ReminderEmail
     {
-        public static void Remind(string url)
+        public static void Remind(string url, int j)
         {
+
+            //logika aplikacji? - ??? 
+            //Bo t
 
             var endTime = GetEndData.GetTime(url); //Konczy sie 08 listopada licytacja
 
-            var ParsedTime = DateTime.Parse(endTime[0]) - DateTime.Now;
+            var ParsedTime = DateTime.Parse(endTime[j]) - DateTime.Now;
 
-            var AcctualDate = DateTime.Now; //3.11
+            DateTime endTimeParsed = DateTime.Parse(endTime[j]);
+
+            DateTime acctualDate = DateTime.Now; //3.11
                                             //Więc jesli data terazniejsza bedzie 7.11 to ma wypisac ze blisko 
 
-            Console.WriteLine("tutaj odjety czas " + ParsedTime.ToString() + GetOffertsHrefs.GetHrefs(url).ToString());
+            var HowManyLeft = (endTimeParsed - acctualDate).Ticks;
 
+            
+            //Sprawdzamy czy data jest rowna jakiejś liczbie
+            //np. Jesli data licytacji "19.11" - "15.11" jest równa 1 to aukcja sie zaraz kończy a jak jest równa innej to aukcja trwa jeszcze kilka dni
 
-
+            if (HowManyLeft.Equals(0))
+            {
+                Console.WriteLine("Licytacja dobiega końca");
+            }
+            {
+                Console.WriteLine("Licytacja jeszcze będzie trwać kilka dni");
+            }
         }
     }
 }
